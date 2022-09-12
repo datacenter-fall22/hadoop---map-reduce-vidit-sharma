7cb761e6e4e7701ca150b56c2a06e080e6559c86
Vidit Vivek Sharma <88590113+vidit-sharma@users.noreply.github.com>
2022-09-11 18:42:18 -0600

    Add files via upload

--- reducer.py
+++ reducer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-"""reducer.py"""
 
 from operator import itemgetter
 import sys
@@ -8,33 +7,31 @@ current_word = None
 current_count = 0
 word = None
 
-# input comes from STDIN
+# read the entire line from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+	# remove leading and trailing whitespace
+	line = line.strip()
+	# splitting the data on the basis of tab we have provided in mapper.py
+	word, count = line.split('\t', 1)
+	# convert count (currently a string) to int
+	try:
+		count = int(count)
+	except ValueError:
+		# count was not a number, so silently
+		# ignore/discard this line
+		continue
 
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
-
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
+	# this IF-switch only works because Hadoop sorts map output
+	# by key (here: word) before it is passed to the reducer
+	if current_word == word:
+		current_count += count
+	else:
+		if current_word:
+			# write result to STDOUT
+			print '%s\t%s' % (current_word, current_count)
+		current_count = count
+		current_word = word
 
 # do not forget to output the last word if needed!
 if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+	print '%s\t%s' % (current_word, current_count)
